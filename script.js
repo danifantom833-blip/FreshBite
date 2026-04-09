75cf2d728cffcb6db814553ee42e0018fe52cccd
danifantom833-blip <danifantom833@gmail.com>
2026-04-09 17:08:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -413,3 +413,10 @@ function initPage() {
 
 window.addEventListener('DOMContentLoaded', initPage);
 
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
